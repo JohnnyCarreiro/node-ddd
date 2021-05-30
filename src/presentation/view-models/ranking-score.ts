d948cd6d6bf868c56b40eaecc636a3459ff0e4e1
Johnny <jcarreiropublicidade@gmail.com>
2021-05-29 21:54:07 -0300

    feat: Create map method to export ranking socre view model

--- src/presentation/view-models/ranking-score.ts
+++ src/presentation/view-models/ranking-score.ts
@@ -1,8 +1,21 @@
-export type RankingScoreViewModel = {
-  player: Player
-  score: number
-  matchDate: string
-  heroes: Hero[]
+import { RankingScoreModel } from '@data/models'
+
+export class RankingScoreViewModel {
+  player!: Player
+  score!: number
+  matchDate!: string
+  heroes!: Hero[]
+
+  static map (entity: RankingScoreModel): RankingScoreViewModel {
+    return {
+      ...entity,
+      matchDate: entity.matchDate.toISOString()
+    }
+  }
+
+  static mapColection (entities: RankingScoreModel[]): RankingScoreViewModel[] {
+    return entities.map(entity => RankingScoreViewModel.map(entity))
+  }
 }
 type Player = {
   name: string
