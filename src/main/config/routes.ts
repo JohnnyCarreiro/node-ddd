6c82449a5843f67ddeac6f72b26d244f7411f753
Johnny <jcarreiropublicidade@gmail.com>
2021-05-31 11:31:19 -0300

    fix: Fix route enter point

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export const setupRoutes = (app: Express): void => {
   const router = Router()
-  app.use('api', router)
+  app.use('/api', router)
   readdirSync(path.join(__dirname, '..', 'routes')).map(async fileName => {
     (await import(`../routes/${fileName}`)).default(router)
   })
