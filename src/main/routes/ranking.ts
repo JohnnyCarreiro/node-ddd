b40b7525aa6b933c479825ba0b5261ecdd6f9f5a
Johnny <jcarreiropublicidade@gmail.com>
2021-05-31 11:08:12 -0300

    feat: Add Express Route Adapter

--- src/main/routes/ranking.ts
+++ src/main/routes/ranking.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express'
 
 import { makeLoadLastRankingController } from '@main/factories'
+import { routeAdapter } from '@main/adapters'
 
 export default (router: Router): void => {
-  router.get('/ranking/last', async (req, res) => {
-    const controller = makeLoadLastRankingController()
-    const httpResponse = await controller.handle()
-
-    res.status(httpResponse.statusCode).json(httpResponse.data)
-  })
+  router.get('/ranking/last', routeAdapter(makeLoadLastRankingController()))
 }
